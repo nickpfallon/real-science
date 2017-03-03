13884f4da34393eddef82c6af5dd1eb6c1ed0e96
Nick Fallon <nickfallonwastaken@gmail.com>
2017-03-03 11:41:39 -0700

    More things

---
+++ main.js
@@ -0,0 +1,8 @@
+$ (document).ready(function() {
+  cats()
+})
+
+function cats () {
+$ ("btn").click(function() {
+  $ .get("https://www.reddit.com/r/shittyaskscience/comments/5x7aun/whats_the_melting_point_of_cats_a_friend_wants_to/")
+})}
